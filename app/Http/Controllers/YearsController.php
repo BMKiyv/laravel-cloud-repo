62132b5ace79c078a5198d15449747540bdfa18b
BMKiyv <k1brusenets@gmail.com>
2023-01-06 10:02:03 +0200

    first optimization_v1.1

--- app/Http/Controllers/YearsController.php
+++ app/Http/Controllers/YearsController.php
@@ -3,26 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Storage;
-use Illuminate\Http\Request;
 use App\Http\Requests\UpdateYearsRequest;
-use Illuminate\Support\Facades\DB;
 use App\Models\Year;
 use App\Models\Std;
 
 class YearsController extends Controller
 {
     public function years () {
-        //$years = DB::select('select * from years');
         $years = Year::all();
         return view('years.index', ['years'=>$years]);
     }
     public function year ($year) {
-        $show_year = DB::select("select * from years where years.year=$year");
-        $id_year = get_object_vars($show_year[0])['id'];
-       $show_std = DB::select("select * from stds where stds.years_id=$id_year");
-
-       $show_std = Year::where('years_id', $id_year)->get();
-        return view('years.year', compact('show_std'));
+       $_year = Year::where('year','=', $year)->get()[0];
+       $show_year = $_year->year;
+       $id_year = $_year->id;
+       $show_std = Std::where('years_id','=', $id_year)->get();
         return view('years.year', ['std'=>$show_std, 'year'=>$show_year]);
     }
     public function update (UpdateYearsRequest $request, $year) {
--- resources/views/years/year.blade.php
+++ resources/views/years/year.blade.php
@@ -1,25 +1,25 @@
 @extends('layouts.main')
-@section('title', 'Year ' . $year[0]->year)
+@section('title', 'Year ' . $year)
 @section('content')
-<div>Підприємства зареєстровані в {{$year[0]->year}}:</div>
+<div>Підприємства зареєстровані в {{$year}}:</div>
 @if (!$std)
     <div>Мабуть, в базу даних щось забув загрузити! Чи ще немає підприємств?!</div>
     <img src="/images/whatever.png" alt="whatever">
 @else
     @foreach ($std as $el)
     <div class="data-container">
-       <a href="{{ '/years' . '/' . $year[0]->year . '/' . $el->name}}"> <div>{{$el->name}}</div></a>
+       <a href="{{ '/years' . '/' . $year . '/' . $el->name}}"> <div>{{$el->name}}</div></a>
     </div>
 @endforeach
 @endif
 <h3>Створити нове зареєстроване підрпиємство</h3>
-<form class="sendform" action="{{route('years-update',$year[0]->year)}}" method="post">
+<form class="sendform" action="{{route('years-update',$year)}}" method="post">
     @csrf
     <div class="form-group">
         <label for="name">Назва папки</label>
         <input type="text" class="form-control" id="name" name="name" value="" placeholder="Введіть назву папки">
     </div>
-    <a href="{{route('years-update',$year[0]->year)}}"><button type="submit" class="btn btn-primary mt-3">Створити</button></a>
+    <a href="{{route('years-update',$year)}}"><button type="submit" class="btn btn-primary mt-3">Створити</button></a>
     {{-- <a href="{{route('formdata-update',$data->id)}}"><button type="submit" class="btn btn-primary mt-3">Редагувати</button></a> --}}
 </form>
 @endsection
